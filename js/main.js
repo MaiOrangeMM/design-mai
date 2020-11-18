d18f657c56599d2740df0c8abcf17aea83180588
MayElectric <42119515+MayElectric@users.noreply.github.com>
2020-11-18 21:21:51 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////
+// Toggle Buttons
+/////////////////////////////////////////////////////
+const menuItems = document.querySelector('.side-nav');
+const menuSpan = document.querySelector('.menu-span');
+const menuBtn = document.querySelector('.menu-btn');
+const menuItem = document.querySelectorAll('.menu-item');
+
+menuBtn.addEventListener('click', () => {
+    toggle();
+});
+
+menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+        if(menuBtn.classList.contains('open')) {
+            toggle();
+        }
+    });
+});
+
+function toggle() {
+    menuSpan.classList.toggle("open");
+    menuBtn.classList.toggle("open");
+    menuItems.classList.toggle("open");
+}
